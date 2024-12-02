b9e490b6061fc7ca335ebc24e69eda17a06f704a
siva-r767 <167684678+sivarajesh767@users.noreply.github.com>
2024-12-02 13:32:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component{
       <Switch>
       <Route exact path="/login" component={Login}/>
       <ProtectedRoute exact path="/" component={Home}/>
-      <ProtectedRoute exact path="jobs" component={Jobs}/>
+      <ProtectedRoute exact path="/jobs" component={Jobs}/>
       <ProtectedRoute exact path="jobs/:id" component={JobItemDetails}/>
       <Route path="/not found" component={NotFound}/>
       <Redirect to="not-found"/>
